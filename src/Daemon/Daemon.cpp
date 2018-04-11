8a7274993c55748469d73619f8b7127c432ce21f
lilsiz <sisma12345.dt@gmail.com>
2018-04-11 18:36:00 -0400

    Update Daemon.cpp

--- src/Daemon/Daemon.cpp
+++ src/Daemon/Daemon.cpp
@@ -42,7 +42,7 @@ namespace
   const command_line::arg_descriptor<std::string> arg_config_file = {"config-file", "Specify configuration file", std::string(CryptoNote::CRYPTONOTE_NAME) + ".conf"};
   const command_line::arg_descriptor<bool>        arg_os_version  = {"os-version", ""};
   const command_line::arg_descriptor<std::string> arg_log_file    = {"log-file", "", ""};
-  const command_line::arg_descriptor<int>         arg_log_level   = {"log-level", "", 2}; // info level
+  const command_line::arg_descriptor<int>         arg_log_level   = {"log-level", "", 1}; // info level
   const command_line::arg_descriptor<bool>        arg_console     = {"no-console", "Disable daemon console commands"};
   const command_line::arg_descriptor<bool>        arg_testnet_on  = {"testnet", "Used to deploy test nets. Checkpoints and hardcoded seeds are ignored, "
     "network id is changed. Use it with --data-dir flag. The wallet must be launched with --testnet flag.", false};
